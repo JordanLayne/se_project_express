d5d25847e50c8b02519d5b4b2a3916c62d7b914c
Jordan Layne <jordanmlayne2001@gmail.com>
2023-04-27 07:31:22 -0400

    Project 13

--- controllers/user.js
+++ controllers/user.js
@@ -2,10 +2,6 @@ const bcrypt = require("bcryptjs");
 
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
-
-const JWT_SECRET  = require("../utils/config");
-
 const {
   ERROR_DOES_NOT_EXIST,
   INVALID_DATA_CODE,
@@ -15,36 +11,17 @@ const {
   UNAUTHORIZED_CODE,
 } = require("../utils/errors");
 
+const { JWT_SECRET } = require("../utils/config");
+
+const User = require("../models/user");
 
 module.exports = {
   async getUsers(req, res) {
-    try {
-      const users = await User.find({});
-      res.send(users);
-    } catch (error) {
-      res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-    }
-  },
-
-  async findUser(req, res) {
-    try {
-      const user = await User.findById(req.params.userId).orFail(() => {
-        throw ERROR_DOES_NOT_EXIST;
-      });
-      res.send({ data: user });
-    } catch (error) {
-      if (error.statusCode === DOES_NOT_EXIST_CODE) {
-        res.status(DOES_NOT_EXIST_CODE).send({
-          message: "Requested data could not be found",
-        });
-      } else if (error.name === "CastError") {
-        res.status(INVALID_DATA_CODE).send({
-          message: "Id provided was invalid",
-        });
-      } else {
+    User.find({})
+      .then((users) => res.send(users))
+      .catch(() => {
         res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-      }
-    }
+      });
   },
 
   async createUser(req, res) {
@@ -57,12 +34,12 @@ module.exports = {
           .then((user) => {
             res.send({ name, avatar, email, _id: user._id });
           })
-          .catch((error) => {
-            if (error.name === "ValidationError") {
+          .catch((err) => {
+            if (err.name === "ValidationError") {
               res.status(INVALID_DATA_CODE).send({
                 message: "Data provided is invalid",
               });
-            } else if (error.code === 11000) {
+            } else if (err.code === 11000) {
               res
                 .status(CONFLICT_CODE)
                 .send({ message: "User with this email already exists" });
@@ -121,12 +98,12 @@ module.exports = {
         throw ERROR_DOES_NOT_EXIST;
       })
       .then((user) => res.send(user))
-      .catch((error) => {
-        if (error.name === "ValidationError") {
+      .catch((err) => {
+        if (err.name === "ValidationError") {
           res.status(INVALID_DATA_CODE).send({
             message: "Data provided is invalid",
           });
-        } else if (error.statusCode === DOES_NOT_EXIST_CODE) {
+        } else if (err.statusCode === DOES_NOT_EXIST_CODE) {
           res.status(DOES_NOT_EXIST_CODE).send({
             message: "Requested data could not be found",
           });

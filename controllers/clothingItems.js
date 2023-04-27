91e3216c9fc46c052511dbd9a49801464c7ff9d8
Jordan Layne <jordanmlayne2001@gmail.com>
2023-04-27 06:48:06 -0400

    Project 13

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,75 +1,94 @@
+const Item = require("../models/clothingItems");
 
-const Item = require('../models/clothingItems');
-
-const User = require('../models/user');
 
 const {
   ERROR_DOES_NOT_EXIST,
   INVALID_DATA_CODE,
   DOES_NOT_EXIST_CODE,
   DEFAULT_CODE,
-  UNAUTHORIZED_CODE
-} = require('../utils/errors');
-
-const handleErrors = (err, res) => {
-  if (err.statusCode === DOES_NOT_EXIST_CODE) {
-    res.status(DOES_NOT_EXIST_CODE).send({ message: 'Requested data could not be found' });
-  } else if (err.name === 'CastError' || err.name === 'ValidationError') {
-    res.status(INVALID_DATA_CODE).send({ message: 'Invalid data provided' });
-  } else {
-    res.status(DEFAULT_CODE).send({ message: 'Error with the server' });
-  }
-};
+  UNAUTHORIZED_CODE,
+} = require("../utils/errors");
 
 module.exports.getClothing = (req, res) => {
   Item.find({})
     .then((items) => res.send(items))
-    .catch((err) => handleErrors(err, res));
+    .catch(() => {
+      res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+    });
 };
-
 module.exports.removeClothing = (req, res) => {
-  const { itemId } = req.params;
-  const { _id: userId } = req.user;
+  const owner = req.user._id;
 
-  Item.findById(itemId)
-    .then((item) => {
-      if (!item) {
-        throw ERROR_DOES_NOT_EXIST;
-      }
-
-      if (item.owner.toString() !== userId.toString()) {
-        res.status(UNAUTHORIZED_CODE).send({ message: 'You are not authorized to delete this item' });
-      } else {
-        return Item.findByIdAndDelete(itemId);
-      }
+  Item.findById(req.params.itemId)
+    .orFail(() => {
+      throw ERROR_DOES_NOT_EXIST;
     })
-    .then((deletedItem) => res.send({ data: deletedItem }))
-    .catch((err) => handleErrors(err, res));
+    .then((item) => {
+      if (String(item.owner) !== owner) {
+        return res.status(UNAUTHORIZED_CODE).send({
+          message: "You do not have permission to delete this resource",
+        });
+      }
+
+      return item.deleteOne().then(() => res.send({ data: item }));
+    })
+    .catch((err) => {
+      if (err.statusCode === DOES_NOT_EXIST_CODE) {
+        res.status(DOES_NOT_EXIST_CODE).send({
+          message: "Requested data could not be found",
+        });
+      } else if (err.name === "CastError") {
+        res.status(INVALID_DATA_CODE).send({
+          message: "Id provided was invalid",
+        });
+      } else {
+        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+      }
+    });
 };
 
 module.exports.addClothing = (req, res) => {
   const { name, weather, imageUrl } = req.body;
-  const { _id: owner } = req.user;
+  const owner = req.user._id;
 
   Item.create({ name, weather, imageUrl, owner })
     .then((item) => {
       res.status(201);
       res.send({ data: item });
     })
-    .catch((err) => handleErrors(err, res));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(INVALID_DATA_CODE).send({
+          message: "Data provided is invalid",
+        });
+      } else {
+        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+      }
+    });
 };
-
 module.exports.likeItem = (req, res) => {
   Item.findByIdAndUpdate(
     req.params.itemId,
     { $addToSet: { likes: req.user._id } },
-    { new: true, runValidators: true }
+    { new: true }
   )
     .orFail(() => {
       throw ERROR_DOES_NOT_EXIST;
     })
     .then((item) => res.send({ data: item }))
-    .catch((err) => handleErrors(err, res));
+    .catch((err) => {
+      if (err.statusCode === DOES_NOT_EXIST_CODE) {
+        res.status(DOES_NOT_EXIST_CODE).send({
+          message: "Requested data could not be found",
+        });
+      } else if (err.name === "CastError") {
+        res.status(INVALID_DATA_CODE).send({
+          message: "Id provided was invalid",
+        });
+      } else {
+        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+      }
+    });
 };
 
 module.exports.dislikeItem = (req, res) => {
@@ -82,32 +101,18 @@ module.exports.dislikeItem = (req, res) => {
       throw ERROR_DOES_NOT_EXIST;
     })
     .then((item) => res.send({ data: item }))
-    .catch((err) => handleErrors(err, res));
+    .catch((err) => {
+      if (err.statusCode === DOES_NOT_EXIST_CODE) {
+        res.status(DOES_NOT_EXIST_CODE).send({
+          message: "Requested data could not be found",
+        });
+      } else if (err.name === "CastError") {
+        res.status(INVALID_DATA_CODE).send({
+          message: "Id provided was invalid",
+        });
+      } else {
+        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+      }
+    });
 };
 
-module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => handleErrors(err, res));
-};
-
-module.exports.findUser = (req, res) => {
-  User.findById(req.params.userId)
-    .orFail(() => {
-      throw ERROR_DOES_NOT_EXIST;
-    })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => handleErrors(err, res));
-};
-
-
-module.exports.createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  User.create({ name, avatar })
-    .then((user) => res.send(user))
-    .catch((err) => handleErrors(err, res));
-};
-
-module.exports.ERROR_DOES_NOT_EXIST = ERROR_DOES_NOT_EXIST;
-module.exports.INVALID_DATA_CODE = INVALID_DATA_CODE;

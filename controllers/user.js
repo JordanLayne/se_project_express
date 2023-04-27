52cdcabdd040b8b11cc3ce1f7d7c415cd14053b5
Jordan Layne <jordanmlayne2001@gmail.com>
2023-04-27 06:56:15 -0400

    Project 13

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ const {
   DOES_NOT_EXIST_CODE,
   DEFAULT_CODE,
   CONFLICT_CODE,
-  UNAUTHORIZED_CODE
+  UNAUTHORIZED_CODE,
 } = require("../utils/errors");
 
 const { JWT_SECRET } = require("../utils/config");
@@ -17,67 +17,42 @@ const User = require("../models/user");
 
 module.exports = {
   async getUsers(req, res) {
-    try {
-      const users = await User.find({});
-      res.send(users);
-    } catch (error) {
-      res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-    }
+    User.find({})
+      .then((users) => res.send(users))
+      .catch(() => {
+        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+      });
   },
 
-  async findUser(req, res) {
-    try {
-      const user = await User.findById(req.params.userId).orFail(() => {
-        throw ERROR_DOES_NOT_EXIST;
-      });
-      res.send({ data: user });
-    } catch (error) {
-      if (error.statusCode === DOES_NOT_EXIST_CODE) {
-        res.status(DOES_NOT_EXIST_CODE).send({
-          message: "Requested data could not be found",
-        });
-      } else if (error.name === "CastError") {
-        res.status(INVALID_DATA_CODE).send({
-          message: "Id provided was invalid",
-        });
-      } else {
-        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-      }
-    }
-  },
   async createUser(req, res) {
-    try {
-      const {
-        name = "Elise Bouer",
-        avatar = "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
-        email,
-        password,
-      } = req.body;
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        res.status(CONFLICT_CODE).send({
-          message: "User with this email already exists",
-        });
-      }
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await User.create({
-        name,
-        avatar,
-        email,
-        password: hashedPassword,
-      });
+    const { name, avatar, email, password } = req.body;
 
-      const { _id, name: userName, avatar: userAvatar, email: userEmail } = user;
-      res.send({ data: { _id, name: userName, avatar: userAvatar, email: userEmail } });
-    } catch (error) {
-      if (error.name === "ValidationError") {
-        res.status(INVALID_DATA_CODE).send({
-          message: "Data provided is invalid",
-        });
-      } else {
+    bcrypt
+      .hash(password, 10)
+      .then((hash) => {
+        User.create({ name, avatar, email, password: hash })
+          .then((user) => {
+            res.send({ name, avatar, email, _id: user._id });
+          })
+          .catch((err) => {
+            if (err.name === "ValidationError") {
+              res.status(INVALID_DATA_CODE).send({
+                message: "Data provided is invalid",
+              });
+            } else if (err.code === 11000) {
+              res
+                .status(CONFLICT_CODE)
+                .send({ message: "User with this email already exists" });
+            } else {
+              res
+                .status(DEFAULT_CODE)
+                .send({ message: "Error with the server" });
+            }
+          });
+      })
+      .catch(() => {
         res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-      }
-    }
+      });
   },
 
   async login(req, res) {
@@ -91,51 +66,50 @@ module.exports = {
         res.send({ token });
       })
       .catch((error) => {
-        res.status(UNAUTHORIZED_CODE).send({ message: error.message});
+        res.status(UNAUTHORIZED_CODE).send({ message: error.message });
       });
   },
 
   async getCurrentUser(req, res) {
     User.findById(req.user._id)
-    .orFail(() => {
-      throw ERROR_DOES_NOT_EXIST;
-    })
-    .then((user) => res.send({ data: user }))
-    .catch((error) => {
-      if (error.statusCode === DOES_NOT_EXIST_CODE) {
-        res.status(DOES_NOT_EXIST_CODE).send({
-          message: "Requested data could not be found",
-        });
-      } else if (error.name === "CastError") {
-        res.status(INVALID_DATA_CODE).send({
-          message: "Id provided was invalid",
-        });
-      } else {
-        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-      }
-    });
-},
-  async updateUserProfile(req, res) {
-    try {
-      const { name, avatar } = req.body;
-      const user = await User.findByIdAndUpdate(
-        req.user._id,
-        { name, avatar },
-        { new: true, runValidators: true }
-      );
-      res.send({ data: user });
-    } catch (error) {
-      if (error.name === "ValidationError") {
-        res.status(INVALID_DATA_CODE).send({
-          message: "Data provided is invalid",
-        });
-      } else if (error.statusCode === DOES_NOT_EXIST_CODE) {
-        res.status(DOES_NOT_EXIST_CODE).send({
-          message: "Requested data could not be found",
-        });
-      } else {
-        res.status(DEFAULT_CODE).send({ message: "Error with the server" });
-      }
-    }
+      .orFail(() => {
+        throw ERROR_DOES_NOT_EXIST;
+      })
+      .then((user) => res.send({ data: user }))
+      .catch((error) => {
+        if (error.statusCode === DOES_NOT_EXIST_CODE) {
+          res.status(DOES_NOT_EXIST_CODE).send({
+            message: "Requested data could not be found",
+          });
+        } else if (error.name === "CastError") {
+          res.status(INVALID_DATA_CODE).send({
+            message: "Id provided was invalid",
+          });
+        } else {
+          res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+        }
+      });
+  },
+  async updateUser(req, res) {
+    const { name, avatar } = req.body;
+
+    User.findByIdAndUpdate(req.user._id, { name, avatar })
+      .orFail(() => {
+        throw ERROR_DOES_NOT_EXIST;
+      })
+      .then((user) => res.send(user))
+      .catch((err) => {
+        if (err.name === "ValidationError") {
+          res.status(INVALID_DATA_CODE).send({
+            message: "Data provided is invalid",
+          });
+        } else if (err.statusCode === DOES_NOT_EXIST_CODE) {
+          res.status(DOES_NOT_EXIST_CODE).send({
+            message: "Requested data could not be found",
+          });
+        } else {
+          res.status(DEFAULT_CODE).send({ message: "Error with the server" });
+        }
+      });
   },
 };

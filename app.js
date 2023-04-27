e4490aeabd91d84e7e71de09eb4803eed4a6e004
Jordan Layne <jordanmlayne2001@gmail.com>
2023-04-27 07:34:34 -0400

    Project 13

--- app.js
+++ app.js
@@ -28,13 +28,6 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '...'
-  };
-
-  next();
-});
 
 app.use(limiter);
 

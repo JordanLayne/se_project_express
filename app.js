f19d6fdb182d90ab713f3fea797aaff6fb235d5a
Jordan Layne <jordanmlayne2001@gmail.com>
2023-05-12 22:04:07 -0400

    Project 15

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const limiter = require("./rateLimitConfig");
+const limiter = require("./utils/config");
 const { errors } = require("celebrate");
 const helmet = require("helmet");
 const cors = require("cors");
